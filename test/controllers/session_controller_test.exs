ed99c64bd9fc49af0e81def9996ba5579df1d073
Christoph Rüßler <christoph.ruessler@mailbox.org>
2015-07-28 23:56:58 +0200

    Remove unused import

--- test/controllers/session_controller_test.exs
+++ test/controllers/session_controller_test.exs
@@ -1,8 +1,6 @@
 defmodule LaFamiglia.SessionControllerTest do
   use LaFamiglia.ConnCase
 
-  alias LaFamiglia.Player
-
   test "GET /sessions/new" do
     conn = get conn(), "/sessions/new"
     assert html_response(conn, 200) =~ "Log in"
