54c1a483dc8688e75a98132c660573b7fdefcccb
Christoph Rüßler <christoph.ruessler@mailbox.org>
2015-07-29 00:12:06 +0200

    Fix failing test

--- test/controllers/session_controller_test.exs
+++ test/controllers/session_controller_test.exs
@@ -1,8 +1,8 @@
 defmodule LaFamiglia.SessionControllerTest do
   use LaFamiglia.ConnCase
 
-  test "GET /sessions/new" do
-    conn = get conn(), "/sessions/new"
+  test "GET /session/new" do
+    conn = get conn(), "/session/new"
     assert html_response(conn, 200) =~ "Log in"
   end
 end
