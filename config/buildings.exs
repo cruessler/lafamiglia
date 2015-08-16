295d4674d4f70ed5b860bc5a3ffb3fc6c62e9269
Christoph Rüßler <christoph.ruessler@mailbox.org>
2015-08-16 20:48:08 +0200

    Change configuration format

--- config/buildings.exs
+++ config/buildings.exs
@@ -2,27 +2,31 @@ use Mix.Config
 
 config :la_famiglia,
   buildings: [
-    building_1: [
+    building_1: %{
       id: 1,
       build_time: fn level -> level * 1 + 4 end,
-      costs: [
-        resource_1: fn level -> level * 1 + 1 end,
-        resource_2: fn level -> level * 1 + 1 end,
-        resource_3: fn level -> level * 1 + 1 end
-        ],
+      costs: fn level ->
+        %{
+        resource_1: level * 1 + 1,
+        resource_2: level * 1 + 1,
+        resource_3: level * 1 + 1
+        }
+      end,
       maxlevel: 8,
       defense: fn level -> 10 end,
       points: fn level -> :math.pow(level, 1.5) end
-      ],
-    building_2: [
+      },
+    building_2: %{
       id: 2,
       build_time: fn level -> level * 1 + 4 end,
-      costs: [
-        resource_1: fn level -> level * 1 + 1 end,
-        resource_2: fn level -> level * 1 + 1 end,
-        resource_3: fn level -> level * 1 + 1 end
-        ],
+      costs: fn level ->
+        %{
+        resource_1: level * 1 + 1,
+        resource_2: level * 1 + 1,
+        resource_3: level * 1 + 1
+        }
+      end,
       maxlevel: 8,
       defense: fn level -> 0 end,
       points: fn level -> :math.pow(level, 1.5) end
-      ]]
+      }]
