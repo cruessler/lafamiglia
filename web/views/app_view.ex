4c69a53ca13f8a77c790103dff48e2337b8817ea
Christoph Rüßler <christoph.ruessler@mailbox.org>
2015-08-16 20:49:31 +0200

    Generalize helper function

--- web/views/app_view.ex
+++ web/views/app_view.ex
@@ -10,7 +10,7 @@ defmodule LaFamiglia.AppView do
     ""
   end
 
-  def resources(%Villa{resource_1: resource_1, resource_2: resource_2, resource_3: resource_3}) do
+  def resources(%{resource_1: resource_1, resource_2: resource_2, resource_3: resource_3}) do
     content_tag :div, class: "resources" do
       "R1: #{resource_1}, R2: #{resource_2}, R3: #{resource_3}"
     end
