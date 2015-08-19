0097514c20be0608de8e14faa051d5acc40736e4
Christoph Rüßler <christoph.ruessler@mailbox.org>
2015-08-19 21:30:55 +0200

    Add Building.virtual_level

--- test/models/building_queue_item_test.exs
+++ test/models/building_queue_item_test.exs
@@ -4,13 +4,18 @@ defmodule LaFamiglia.BuildingQueueItemTest do
   alias LaFamiglia.BuildingQueueItem
 
   test "should add building queue item" do
-    villa = Forge.saved_villa(Repo)
+    villa    = Forge.saved_villa(Repo)
+    building = Building.get_by_id(1)
+
+    assert Building.virtual_level(villa, building) == 1
 
     for i <- 1..3 do
-      villa |> BuildingQueueItem.enqueue(Building.get_by_id(1))
+      villa |> BuildingQueueItem.enqueue(building)
       items = assoc(villa, :building_queue_items) |> Repo.all
 
       assert Enum.count(items) == i
     end
+
+    assert Building.virtual_level(villa, building) == 4
   end
 end
--- web/models/building.ex
+++ web/models/building.ex
@@ -1,4 +1,6 @@
 defmodule Building do
+  alias LaFamiglia.Repo
+
   def get_by_id(id) do
     {_k, b} = Enum.find Application.get_env(:la_famiglia, :buildings), fn({_k, b}) ->
       b.id == id
@@ -8,5 +10,17 @@ defmodule Building do
 
   def level(villa, building) do
     Map.get(villa, building.key)
+  end
+
+  def virtual_level(villa, building) do
+    level(villa, building) + enqueued_count(villa, building)
+  end
+
+  defp enqueued_count(villa, building) do
+    Ecto.Model.assoc(villa, :building_queue_items)
+    |> Repo.all
+    |> Enum.count fn(item) ->
+      item.building_id == building.id
+    end
   end
 end
