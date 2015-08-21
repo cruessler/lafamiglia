5df92b2c8835eccdfa9a2f03e7ccfca2a712a86d
Christoph Rüßler <christoph.ruessler@mailbox.org>
2015-08-22 00:01:23 +0200

    Add comment

--- lib/date_time.ex
+++ lib/date_time.ex
@@ -1,4 +1,12 @@
 defmodule LaFamiglia.DateTime do
+  @moduledoc """
+  This module manages the game’s time. It does so by providing the function
+  `now/0` which returns the same value for subsequent calls until `clock!/1`
+  is called. `clock!/1` will typically be called at the beginning of a request
+  or test, or before an event is processed by the backend. That way, the game’s
+  time does not change across multiple calls of `now/0`.
+  """
+
   def to_seconds %Ecto.DateTime{usec: usecs} = datetime do
     datetime
     |> Ecto.DateTime.to_erl
