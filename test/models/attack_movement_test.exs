80615362c5ac9d52938b94c118d28ed150835178
Christoph Rüßler <christoph.ruessler@mailbox.org>
2015-10-09 21:11:27 +0200

    Add validations to AttackMovement

--- test/models/attack_movement_test.exs
+++ test/models/attack_movement_test.exs
@@ -2,4 +2,33 @@ defmodule LaFamiglia.AttackMovementTest do
   use LaFamiglia.ModelCase
 
   alias LaFamiglia.AttackMovement
+
+  test "should respect validations" do
+    origin_without_units = Forge.saved_villa(Repo)
+    origin = Forge.saved_villa(Repo, unit_1: 10)
+    target = Forge.saved_villa(Repo)
+
+    movement_params = %{origin_id: origin.id,
+                        target_id: target.id,
+                        unit_1: 1,
+                        unit_2: 0}
+
+    changeset = AttackMovement
+                  .changeset(%AttackMovement{}, movement_params)
+    assert changeset.valid?
+
+    changeset = AttackMovement
+                  .changeset(%AttackMovement{}, %{movement_params | unit_1: 0})
+    refute changeset.valid?
+
+    changeset = AttackMovement
+                  .changeset(%AttackMovement{},
+                             %{movement_params | origin_id: origin_without_units.id})
+    refute changeset.valid?
+
+    changeset = AttackMovement
+                  .changeset(%AttackMovement{},
+                             %{movement_params | target_id: origin.id})
+    refute changeset.valid?
+  end
 end
--- web/models/attack_movement.ex
+++ web/models/attack_movement.ex
@@ -1,7 +1,12 @@
 defmodule LaFamiglia.AttackMovement do
   use LaFamiglia.Web, :model
 
+  alias Ecto.Changeset
+
+  alias LaFamiglia.Repo
   alias LaFamiglia.Villa
+
+  alias LaFamiglia.Unit
 
   schema "attack_movements" do
     belongs_to :origin, Villa
@@ -27,5 +32,58 @@ defmodule LaFamiglia.AttackMovement do
   def changeset(model, params \\ :empty) do
     model
     |> cast(params, @required_fields, @optional_fields)
+    |> validate_origin_and_target_are_different
+    |> preload_associations
+    |> validate_origin_and_target_belong_to_different_players
+    |> validate_unit_numbers
+    |> validate_at_least_one_unit
+    |> assoc_constraint(:origin)
+    |> assoc_constraint(:target)
+  end
+
+  defp validate_origin_and_target_are_different(changeset) do
+    case changeset.changes.origin_id == changeset.changes.target_id do
+      true -> add_error(changeset, :target_id, "must be different from origin")
+      _    -> changeset
+    end
+  end
+
+  # So far, there seems to be no standard way of validating fields of associated
+  # models. This is a hack to make `origin` and `target` available to the
+  # validations.
+  defp preload_associations(%Changeset{changes: changes, model: model} = changeset) do
+    model =
+      model
+      |> Map.put(:origin, Repo.get!(Villa, changes.origin_id))
+      |> Map.put(:target, Repo.get!(Villa, changes.target_id))
+    %Changeset{changeset | model: model}
+  end
+
+  defp validate_origin_and_target_belong_to_different_players(changeset) do
+    case get_field(changeset, :origin).player_id == get_field(changeset, :target).player_id do
+      true -> add_error(changeset, :target_id, "must not be owned by you")
+      _    -> changeset
+    end
+  end
+
+  defp validate_unit_numbers(changeset) do
+    origin = get_field(changeset, :origin)
+
+    LaFamiglia.Unit.all
+    |> Enum.reduce changeset, fn({k, u}, changeset) ->
+      changeset
+      |> validate_number(k, less_than_or_equal_to: Unit.number(origin, u))
+    end
+  end
+
+  defp validate_at_least_one_unit(%Changeset{changes: changes} = changeset) do
+    total_unit_number =
+      LaFamiglia.Unit.all
+      |> Enum.reduce 0, fn({k, _u}, acc) -> acc + changes[k] end
+
+    case total_unit_number == 0 do
+      true -> add_error(changeset, :unit_count, "must be greater than 0")
+      _    -> changeset
+    end
   end
 end
