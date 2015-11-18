b984b5e39361cc1a7024b5641e3b1f4c8526daf1
Christoph Rüßler <christoph.ruessler@mailbox.org>
2015-11-18 22:31:51 +0100

    Use plug to load conversations

--- web/controllers/conversation_controller.ex
+++ web/controllers/conversation_controller.ex
@@ -6,22 +6,31 @@ defmodule LaFamiglia.ConversationController do
   alias LaFamiglia.Conversation
   alias LaFamiglia.Message
 
-  def index(conn, _params) do
-    conversations =
-      from(c in assoc(conn.assigns.current_player, :conversations),
-        order_by: [desc: c.last_message_sent_at])
-      |> Repo.all
-      |> Repo.preload([:players])
+  plug :load_conversations
 
+  def index(conn, _params) do
     conn
     |> assign(:conversation, %Conversation{})
-    |> assign(:conversations, conversations)
     |> assign(:changeset, Ecto.Changeset.change(%Message{}))
     |> render("index.html")
   end
 
   def show(conn, %{"id" => id}) do
-    conversation  = Repo.get(Conversation, id) |> Repo.preload([messages: :sender])
+    conversation =
+      Repo.get(Conversation, id)
+      |> Repo.preload([messages: :sender])
+    changeset =
+      Ecto.Changeset.change(%Message{},
+                            %{conversation_id: conversation.id,
+                              sender_id: conn.assigns.current_player.id})
+
+    conn
+    |> assign(:conversation, conversation)
+    |> assign(:changeset, changeset)
+    |> render("show.html")
+  end
+
+  defp load_conversations(conn, _params) do
     conversations =
       from(c in assoc(conn.assigns.current_player, :conversations),
         order_by: [desc: c.last_message_sent_at])
@@ -29,11 +38,6 @@ defmodule LaFamiglia.ConversationController do
       |> Repo.preload([:players])
 
     conn
-    |> assign(:conversation, conversation)
     |> assign(:conversations, conversations)
-    |> assign(:changeset, Ecto.Changeset.change(%Message{},
-                                                %{conversation_id: conversation.id,
-                                                  sender_id: conn.assigns.current_player.id}))
-    |> render("show.html")
   end
 end
