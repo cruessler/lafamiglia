da63fff2f498b45c8e6ccb499f5253b817ab3590
Christoph Rüßler <christoph.ruessler@mailbox.org>
2015-12-30 23:50:59 +0100

    Link reports to villas

--- web/models/combat_report.ex
+++ web/models/combat_report.ex
@@ -1,6 +1,7 @@
 defmodule LaFamiglia.CombatReport do
   alias LaFamiglia.Repo
   alias LaFamiglia.Report
+  alias LaFamiglia.RelatedReportVilla
 
   def deliver!(origin, target, result) do
     report_for_attacker =
@@ -12,7 +13,24 @@ defmodule LaFamiglia.CombatReport do
                                        data: result,
                                        player_id: target.player.id)
 
-    Repo.insert(report_for_attacker)
-    Repo.insert(report_for_defender)
+    report_for_attacker = Repo.insert!(report_for_attacker)
+    report_for_defender = Repo.insert!(report_for_defender)
+
+    RelatedReportVilla.changeset(%RelatedReportVilla{},
+                                 %{related_report_id: report_for_attacker.id,
+                                 villa_id: origin.id})
+    |> Repo.insert!
+    RelatedReportVilla.changeset(%RelatedReportVilla{},
+                                 %{related_report_id: report_for_attacker.id,
+                                 villa_id: target.id})
+    |> Repo.insert!
+    RelatedReportVilla.changeset(%RelatedReportVilla{},
+                                 %{related_report_id: report_for_defender.id,
+                                 villa_id: origin.id})
+    |> Repo.insert!
+    RelatedReportVilla.changeset(%RelatedReportVilla{},
+                                 %{related_report_id: report_for_defender.id,
+                                 villa_id: target.id})
+    |> Repo.insert!
   end
 end
