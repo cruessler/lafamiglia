812a8e94533199db21e29f863511d5317c5ab846
Christoph Rüßler <christoph.ruessler@mailbox.org>
2016-01-02 23:14:00 +0100

    Calculate losses

--- lib/event_loop/combat.ex
+++ lib/event_loop/combat.ex
@@ -17,6 +17,26 @@ defmodule LaFamiglia.Combat do
 
     winner = if attack_value > defense_value, do: :attacker, else: :defender
 
+    case winner do
+      :attacker ->
+        attacker_percent_loss = :math.pow(defense_value / attack_value, 1.5)
+        defender_percent_loss = 1
+      :defender ->
+        attacker_percent_loss = 1
+        defender_percent_loss = :math.pow(attack_value / defense_value, 1.5)
+    end
+
+    attacker_losses =
+      Enum.map(Unit.all, fn({k, u}) ->
+        {k, round(Map.get(attacker, k) * attacker_percent_loss)}
+      end)
+      |> Enum.into(%{})
+    defender_losses =
+      Enum.map(Unit.all, fn({k, u}) ->
+        {k, round(Map.get(defender, k) * defender_percent_loss)}
+      end)
+      |> Enum.into(%{})
+
     %{"attacker" => attacking_units,
       "defender" => defending_units,
       "winner"   => winner}
