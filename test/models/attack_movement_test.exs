5e1e558161a9667515ee0fe3ce092ac6e6665e79
Christoph Rüßler <christoph.ruessler@mailbox.org>
2016-01-22 20:02:55 +0100

    Split test

--- test/models/attack_movement_test.exs
+++ test/models/attack_movement_test.exs
@@ -11,7 +11,13 @@ defmodule LaFamiglia.AttackMovementTest do
       Forge.saved_attack_movement(Repo)
       |> Repo.preload([:origin, :target])
 
-    {:ok, %{attack: attack}}
+    movement_params =
+      %{origin_id: Forge.saved_villa(Repo, unit_1: 10).id,
+        target_id: Forge.saved_villa(Repo).id,
+        unit_1: 1,
+        unit_2: 0}
+
+    {:ok, %{attack: attack, movement_params: movement_params}}
   end
 
   defp report_count do
@@ -29,32 +35,38 @@ defmodule LaFamiglia.AttackMovementTest do
     assert Repo.get(Villa, attack.origin.id).supply < old_supply
   end
 
-  test "should respect validations" do
-    origin_without_units = Forge.saved_villa(Repo)
-    origin = Forge.saved_villa(Repo, unit_1: 10)
-    target = Forge.saved_villa(Repo)
+  test "arrives_at is in the future", %{movement_params: params} do
+    {:ok, movement} =
+      %AttackMovement{}
+      |> AttackMovement.changeset(params)
+      |> Repo.insert
 
-    movement_params = %{origin_id: origin.id,
-                        target_id: target.id,
-                        unit_1: 1,
-                        unit_2: 0}
-
-    changeset = AttackMovement
-                  .changeset(%AttackMovement{}, movement_params)
-    {:ok, movement} = Repo.insert(changeset)
     assert Ecto.DateTime.compare(movement.arrives_at, LaFamiglia.DateTime.now) == :gt
+  end
 
-    changeset = AttackMovement
-                  .changeset(%AttackMovement{}, %{movement_params | unit_1: 0})
+  test "is invalid without units", %{movement_params: params} do
+    changeset =
+      %AttackMovement{}
+      |> AttackMovement.changeset(%{params | unit_1: 0})
+
     refute changeset.valid?
+  end
 
-    changeset = AttackMovement
-                  .changeset(%AttackMovement{},
-                             %{movement_params | target_id: origin.id})
+  test "is invalid when origin == target", %{movement_params: params} do
+    changeset =
+      %AttackMovement{}
+      |> AttackMovement.changeset(%{params | target_id: params.origin_id})
+
     refute changeset.valid?
+  end
 
-    origin_changeset = Ecto.Changeset.change(origin_without_units)
-    changeset        = AttackMovement.changeset(%AttackMovement{}, movement_params)
+  test "is invalide when origin does not have units", %{movement_params: params} do
+    origin           = Forge.saved_villa(Repo)
+    origin_changeset = Ecto.Changeset.change(origin)
+
+    changeset =
+      %AttackMovement{}
+      |> AttackMovement.changeset(%{params | origin_id: origin.id})
 
     assert {:error, _} =
       AttackMovement.attack(origin_changeset, changeset)
