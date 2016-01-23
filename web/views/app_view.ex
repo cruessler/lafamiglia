de94121065ee1b4e7a988cd334b7fe68202faf42
Christoph Rüßler <christoph.ruessler@mailbox.org>
2016-01-23 23:52:09 +0100

    Simplify code

--- web/views/app_view.ex
+++ web/views/app_view.ex
@@ -3,21 +3,15 @@ defmodule LaFamiglia.AppView do
 
   use Phoenix.HTML
 
-  def coordinates(%Villa{x: x, y: y}) do
-    "#{x}|#{y}"
-  end
-  def coordinates(_) do
-    ""
-  end
+  def coordinates(%Villa{x: x, y: y}), do: "#{x}|#{y}"
+  def coordinates(_), do: ""
 
   def resources(%{resource_1: resource_1, resource_2: resource_2, resource_3: resource_3}) do
     content_tag :div, class: "resources" do
       "R1: #{trunc resource_1}, R2: #{trunc resource_2}, R3: #{trunc resource_3}"
     end
   end
-  def resources(_) do
-    ""
-  end
+  def resources(_), do: ""
 
   def player_select(%{name: name}, field) do
     react_component "PlayerSelector", %{name: "#{name}[#{field}]"},
