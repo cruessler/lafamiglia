5acfb321f0a3e9e29e5f8507f2445562a71ba2db
Christoph Rüßler <christoph.ruessler@mailbox.org>
2016-01-29 23:33:41 +0100

    Add ReportControllerTest

---
+++ test/controllers/report_controller_test.exs
@@ -0,0 +1,24 @@
+defmodule LaFamiglia.ReportControllerTest do
+  use LaFamiglia.ConnCase
+
+  setup do
+    player = Forge.saved_player(Repo)
+    conn   = conn |> with_login(player)
+
+    {:ok, %{conn: conn, player: player}}
+  end
+
+  test "GET /reports", %{conn: conn} do
+    conn = get conn, "/reports"
+
+    assert html_response(conn, 200) =~ "Reports"
+  end
+
+  test "GET /reports/1", %{conn: conn, player: player} do
+    report = Forge.saved_report(Repo, player: player)
+
+    conn = get conn, "/reports/#{report.id}"
+
+    assert html_response(conn, 200) =~ report.title
+  end
+end
