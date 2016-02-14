cc0814f47f277b812d2137dbf2c8b3f27daed32b
Christoph Rüßler <christoph.ruessler@mailbox.org>
2016-02-14 20:24:29 +0100

    Add test

--- test/models/message_test.exs
+++ test/models/message_test.exs
@@ -1,14 +1,29 @@
 defmodule LaFamiglia.MessageTest do
   use LaFamiglia.ModelCase
 
+  alias LaFamiglia.Conversation
   alias LaFamiglia.Message
 
-  test "create message" do
+  setup do
     sender_id = Forge.saved_player(Repo).id
     receivers =
       Forge.saved_player_list(Repo, 2)
       |> Enum.map(fn(p) -> %{id: p.id} end)
 
+    message_params = %{sender_id: sender_id,
+                       receivers: receivers,
+                       text: "This is a text."}
+
+    {:ok, %{sender_id: sender_id,
+            receivers: receivers,
+            message_params: message_params}}
+  end
+
+  defp conversation_count do
+    Repo.all(Conversation) |> Enum.count
+  end
+
+  test "create message", %{sender_id: sender_id, receivers: receivers} = context do
     invalid_messages = [
       %{text: "This is a text"}, # has neither sender nor receivers
       %{sender_id: sender_id,
@@ -21,11 +36,18 @@ defmodule LaFamiglia.MessageTest do
                            |> Repo.insert
     end
 
-    message_params = %{sender_id: sender_id,
-                       receivers: receivers,
-                       text: "This is a text."}
-    message = Message.changeset(%Message{}, message_params)
+    message = Message.changeset(%Message{}, context.message_params)
 
     assert {:ok, _} = Repo.insert(message)
+  end
+
+  test "add message to existing conversation", context do
+    old_conversation_count = conversation_count
+
+    message = Message.changeset(%Message{}, context.message_params)
+
+    for _ <- 0..2 do assert {:ok, _} = Repo.insert(message) end
+
+    assert conversation_count == old_conversation_count + 1
   end
 end
