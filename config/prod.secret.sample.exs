36a613ccd275c769a528f1a6f19b5a1e997a9a7e
Christoph Rüßler <christoph.ruessler@mailbox.org>
2016-03-04 20:59:01 +0100

    Add config/prod.secret.sample.exs

---
+++ config/prod.secret.sample.exs
@@ -0,0 +1,15 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you likely want to automate and keep it away from
+# your version control system.
+config :la_famiglia, LaFamiglia.Endpoint,
+  secret_key_base: ""
+
+# Configure your database
+config :la_famiglia, LaFamiglia.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "",
+  password: "",
+  database: "",
+  pool_size: 20
