acc6b1cc4b6c1cde2657b1fb7457a856f86fd1a3
Christoph Rüßler <christoph.ruessler@mailbox.org>
2016-03-13 17:32:14 +0100

    Add test

--- test/models/building_queue_item_test.exs
+++ test/models/building_queue_item_test.exs
@@ -49,4 +49,28 @@ defmodule LaFamiglia.BuildingQueueItemTest do
       |> Villa.changeset(%{building_1: building.maxlevel})
       |> BuildingQueueItem.enqueue!(building)
   end
+
+  test "should update `resources_gained_until` when event is handled", %{changeset: changeset, building: building} do
+    {:ok, villa} = BuildingQueueItem.enqueue!(changeset, building)
+    [first_item] = villa.building_queue_items
+
+    {:ok, villa} =
+      villa
+      |> Ecto.Changeset.change
+      |> BuildingQueueItem.enqueue!(building)
+
+    assert Ecto.Changeset.get_field(changeset, :resources_gained_until) != first_item.completed_at
+
+    LaFamiglia.Event.handle(first_item)
+
+    changeset =
+      from(v in Villa,
+           where: v.id == ^villa.id,
+           preload: [:building_queue_items, :unit_queue_items])
+      |> Repo.one
+      |> Ecto.Changeset.change
+      |> Villa.process_virtually_until(first_item.completed_at)
+
+    assert Ecto.Changeset.get_field(changeset, :resources_gained_until) == first_item.completed_at
+  end
 end
