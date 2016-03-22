eaaa205df265f17e20d08a7a8f2529e48a72dc50
Christoph Rüßler <christoph.ruessler@mailbox.org>
2016-03-22 22:41:29 +0100

    Fix validation

--- web/models/villa.ex
+++ web/models/villa.ex
@@ -110,7 +110,7 @@ defmodule LaFamiglia.Villa do
   defp validate_maxlevel(%{data: villa} = changeset, item) do
     building = Building.get(item.building_id)
 
-    case Building.virtual_level(changeset, building) > building.maxlevel do
+    case Building.virtual_level(changeset, building) >= building.maxlevel do
       true -> add_error(changeset, building.id, "Building already at maxlevel.")
       _    -> changeset
     end
