fa9e8850b7da6c8a1dd8b2573c59a03de991e4e9
Christoph Rüßler <christoph.ruessler@mailbox.org>
2016-03-28 11:30:59 +0200

    Fix Queue.shift_later_items
    Comparison of items has to compare their ids and cannot rely on Elixir’s
    matching.

--- lib/queue.ex
+++ lib/queue.ex
@@ -6,11 +6,12 @@ defmodule LaFamiglia.Queue do
     do: LaFamiglia.DateTime.time_diff(LaFamiglia.DateTime.now, first.completed_at)
   def build_time_left(_, item), do: item.build_time
 
-  def shift_later_items([item], item, time_diff), do: []
-  def shift_later_items([item|rest], item, time_diff),
-    do: shift_items(rest, time_diff)
-  def shift_later_items([first|rest], item, time_diff),
-    do: [first] ++ shift_later_items(rest, item, time_diff)
+  def shift_later_items([first|rest], item, time_diff) do
+    cond do
+      first.id == item.id -> shift_items(rest, time_diff)
+      true -> [first] ++ shift_later_items(rest, item, time_diff)
+    end
+  end
 
   def shift_items([], _), do: []
   def shift_items([first|rest], time_diff),
