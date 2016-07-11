075204335bb0eef0928b9676c16fbaab30486fbd
Christoph Rüßler <christoph.ruessler@mailbox.org>
2016-07-11 18:26:00 +0200

    Add test

--- test/models/comeback_movement_test.exs
+++ test/models/comeback_movement_test.exs
@@ -1,3 +1,14 @@
 defmodule LaFamiglia.ComebackMovementTest do
   use LaFamiglia.ModelCase
+
+  alias LaFamiglia.ComebackMovement
+
+  test "from_attack" do
+    attack = build(:attack)
+
+    comeback = ComebackMovement.from_attack(attack)
+
+    assert get_field(comeback, :origin) == attack.origin
+    assert Ecto.DateTime.compare(get_field(comeback, :arrives_at), attack.arrives_at) == :gt
+  end
 end
