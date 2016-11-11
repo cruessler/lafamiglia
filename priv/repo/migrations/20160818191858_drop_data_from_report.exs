f6650e3b2f1b05ff206e7970335dc9e86e7fc1f1
Christoph Rüßler <christoph.ruessler@mailbox.org>
2016-11-11 21:19:24 +0100

    Make migration work both ways

--- priv/repo/migrations/20160818191858_drop_data_from_report.exs
+++ priv/repo/migrations/20160818191858_drop_data_from_report.exs
@@ -1,9 +1,15 @@
 defmodule LaFamiglia.Repo.Migrations.DropDataFromReport do
   use Ecto.Migration
 
-  def change do
+  def up do
     alter table(:reports) do
       remove :data
+    end
+  end
+
+  def down do
+    alter table(:reports) do
+      add :data, :map
     end
   end
 end
