79c10796d28d5c5bbdf36c150c5c94146fbd39f0
Christoph Rüßler <christoph.ruessler@mailbox.org>
2018-06-26 21:04:16 +0200

    Add core config for Travis

---
+++ config/config.travis.exs
@@ -0,0 +1,31 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# General application configuration
+config :la_famiglia,
+  ecto_repos: [LaFamiglia.Repo]
+
+# Configures the endpoint
+config :la_famiglia, LaFamiglia.Endpoint,
+  url: [host: "localhost"],
+  root: Path.dirname(__DIR__),
+  secret_key_base: String.duplicate("abcdefgh", 8),
+  render_errors: [accepts: ["html"]],
+  pubsub: [name: LaFamiglia.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+config :la_famiglia, game_speed: 1
+config :la_famiglia, unit_speed: 1
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"
