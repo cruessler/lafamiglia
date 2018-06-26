74d5186c056daecdfd08429652e781a6cb52368f
Christoph Rüßler <christoph.ruessler@mailbox.org>
2018-06-26 17:11:24 +0200

    Add Babel presets env and react

--- brunch-config.js
+++ brunch-config.js
@@ -48,6 +48,7 @@ exports.config = {
   // Configure your plugins
   plugins: {
     babel: {
+      presets: ['env', 'react'],
       // Do not use ES6 compiler in vendor code
       ignore: [/^(web\/static\/vendor)/]
     },
